04562c9a160cf07cce0994527b805ba516104b98
Austen <aortmeier20@gmail.com>
2024-04-03 20:56:12 -0500

    insert comments on both begin pypoll

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,20 @@
 import csv
 file_path = "PyBank/Resources/budget_data.csv"
 
+#The total number of months included in the dataset
+
+#The net total amount of "Profit/Losses" over the entire period
+
+#The changes in "Profit/Losses" over the entire period, and then the average of those changes
+
+#The greatest increase in profits (date and amount) over the entire period
+
+#The greatest decrease in profits (date and amount) over the entire period
+
+
+
+
+
 row_number = -1
 sum_balance = -1
 with open(file_path) as text:
@@ -15,3 +29,12 @@ with open(file_path) as text:
         row_number = row_number + 1
 print(row_number)
 print(sum_balance)
+
+
+#Financial Analysis
+#----------------------------
+#Total Months: 86
+#Total: $22564198
+#Average Change: $-8311.11
+#Greatest Increase in Profits: Aug-16 ($1862002)
+#Greatest Decrease in Profits: Feb-14 ($-1825558)
