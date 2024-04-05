0d068408fa475a10a50b42c5df6c4ceb6e02c1a4
Austen <aortmeier20@gmail.com>
2024-04-04 22:15:15 -0500

    created file out still working change and date for increase decrease

--- PyBank/main.py
+++ PyBank/main.py
@@ -17,12 +17,12 @@ file_path = "PyBank/Resources/budget_data.csv"
 
 
 
-
+#create variables
 total_months = 0
 sum_balance = 0
 changes = []
-total_change = 0
 average_change = 0
+total_change = 0
 current_profit_loss = 0
 previous_profit_loss = 0
 
@@ -30,29 +30,49 @@ previous_profit_loss = 0
 with open(file_path) as text:
     csv_file = csv.reader(text)
     next(csv_file) #skips a row in the file ( first row =header row_)
-    for row in csv_file:
-        current_profit_loss = int(row[1]) 
-        total_months = total_months + 1 
-        if total_months >= 1:
-            #calculate total months
+    for row in csv_file: 
+        #add to total months
+        total_months += 1
+        current_profit_loss = int(row[1])
+        if total_months >= 0:
+            #set rows
             Date = row[0]
-            #calculate net total
             profit_loss = row[1]
+            #calculate net total
             sum_balance += int(profit_loss)
-            #calculate change
+        #calculate change
             change = current_profit_loss - previous_profit_loss
             changes.append(change)
             total_change += change
             previous_profit_loss = current_profit_loss
-    average_change = total_change / len(changes)
+    average_change = total_change/len(changes)
+    #greatest increase and greatest decrease
+    greatest_increase = max(changes)
+    #greatest_increase_date = Date[changes.index(greatest_increase)]
+    greatest_decrease = min(changes)
+    #greatest_decrease_date = Date[changes.index(greatest_decrease)]
 
+
+# print to terminal
 print('Financial Analysis')
 print('----------------------------')
 print(f'Total Months: {total_months}')
 print(f'Total: {sum_balance}')
 print(f'Average Change: {average_change}')
+print(f'Greatest Increase in Profits: ${greatest_increase}')
+print(f'Greatest Decrease in Profits: ${greatest_decrease}')
 
 
+#print the results to file
+out_file_path = "PyBank/analysis/financial_analysis_results.txt"
+with open(out_file_path, 'w') as file_out:
+    file_out.write('Financial Analysis\n')
+    file_out.write('----------------------------\n')
+    file_out.write(f'Total Months: {total_months}\n')
+    file_out.write(f'Total: {sum_balance}\n')
+    file_out.write(f'Average Change: {average_change}\n')
+    file_out.write(f'Greatest Increase in Profits: ${greatest_increase}\n')
+    file_out.write(f'Greatest Decrease in Profits: ${greatest_decrease}\n')
 
 #E
 #Financial Analysis
